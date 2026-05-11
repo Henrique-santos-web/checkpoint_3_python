11671a629673217ab4e49e96708c1b808d3c425d
Henrique Miranda <fegemeos4@gmail.com>
2026-05-10 21:24:51 -0300

    passei raiva

--- Conta.py
+++ Conta.py
@@ -5,7 +5,6 @@ tempo = 4
 class Conta():
     def __init__(self):
         self._saldo = 0.0
-        # self.conta = conta
     
     #* Depositar e Sacar
     def get_saldo(self):
@@ -13,16 +12,16 @@ class Conta():
     
 
     def sacar(self, valor):
-        while True:
             try:
-                if valor > self._saldo or valor <= 0.0:
+                if valor > self._saldo:
                     print("Verificando Saldo...")
                     time.sleep(2)
                     os.system('cls' if 'nt' else 'clear')
-                    print("⚠️ ERRO: Apenas Saques acima de 0,01 são autorizados!")
+                    print("Saldo insuficiente!")
                     time.sleep(2)
                     os.system('cls' if 'nt' else 'clear')
-                    continue
+                elif valor <= 0.0:
+                    print("⚠️ ERRO: Apenas Saques acima de 0,01 são autorizados!")
                 else:
                     print("Verificando Saldo...")
                     time.sleep(2)
@@ -31,12 +30,10 @@ class Conta():
                     print(f"Tranferência no valor de: {valor} realizada com sucesso!")
                     time.sleep(2)
                     os.system('cls' if 'nt' else 'clear')
-                    break
             except ValueError:
                 print("Apenas números são permitidos!")
                 time.sleep(2)
                 os.system('cls' if 'nt' else 'clear')
-                continue
 
 
     def depositar(self, valor):
@@ -48,9 +45,6 @@ class Conta():
                     os.system('cls' if 'nt' else 'clear')
                     continue
                 else:
-                    print("Verificando Saldo...")
-                    time.sleep(2)
-                    os.system('cls' if 'nt' else 'clear')
                     self._saldo += valor
                     print(f"Tranferência no valor de: {valor} realizada com sucesso!")
                     break
